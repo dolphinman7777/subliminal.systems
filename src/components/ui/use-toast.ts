a32830559801ab860e9cdd5916fa9cfe741a267f
Jack Davies <i@uos.life>
2024-10-18 22:55:30 +0100

    Fix toast functionality and imports

---
+++ src/components/ui/use-toast.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export interface Toast {
+  id: string;
+  title: string;
+  description?: string;
+  duration?: number;
+}
+
+export function useToast() {
+  const [toasts, setToasts] = useState<Toast[]>([]);
+
+  const addToast = (toast: Omit<Toast, 'id'>) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    setToasts((prevToasts) => [...prevToasts, { ...toast, id }]);
+  };
+
+  const removeToast = (id: string) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  };
+
+  return { toasts, addToast, removeToast };
+}
